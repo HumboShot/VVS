0b753df0dc311f2f3e2a3532e7c7eaf99780c3f3
Christian Glarmbæk <humboshot@gmail.com>
2017-04-10 13:26:14 +0200

    Added all model classes'

---
+++ VVS/VVS/Model/Job.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVS.Model
+{
+    class Job
+    {
+        public int Id { get; set; }
+        public int OldMeterId { get; set; }
+        public int NewMeterId { get; set; }
+        public int BeforeReportId { get; set; }
+        public int AfterReportId { get; set; }
+        public int LocId { get; set; }
+        public bool Status { get; set; }
+
+        public Job(int id, int oldMeterId, int newMeterId, int beforeReportId, int afterReportId, int locId, bool status)
+        {
+            Id = id;
+            OldMeterId = oldMeterId;
+            NewMeterId = newMeterId;
+            BeforeReportId = beforeReportId;
+            AfterReportId = afterReportId;
+            LocId = locId;
+            Status = status;
+        }
+
+        public Job()
+        {
+            
+        }
+    }
+}
---
+++ VVS/VVS/Model/Location.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVS.Model
+{
+    class Location
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+
+        public Location(int id, string address, float longitude, float latitude)
+        {
+            Id = id;
+            Address = address;
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+        public Location()
+        {
+        }
+    }
+}
